7bfa3d4a4279b625520daf0ce0b27af505cd2612
Nicklas <nicklasahrnberg@gmail.com>
2022-06-14 16:13:17 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,21 @@
+xwindow.addEventListener("load", SidenVises);
+
+function SidenVises() {
+    console.log("SidenVises");
+
+    document.querySelector("#burgermenu").addEventListener("click", toggleMenu);
+}
+
+function toggleMenu() {
+    console.log("toggleMenu");
+
+    document.querySelector("#menu").classList.toggle("hidden");
+
+    let erSkjult = document.querySelector("#menu").classList.contains("hidden");
+
+    if (erSkjult == true) {
+        document.querySelector("#burgermenu").innerHTML = "<img src = \"billeder/burgermenu.svg\" alt=\"burger\">";
+    } else {
+        document.querySelector("#burgermenu").innerHTML = "<img src=\"billeder/kryds.svg\" alt = \"kryds\">";
+    }
+}
